61106f804a2762dc1a7a164984391bd13b804ae2
Jonathan McGrath <mcgrath.jon@gmail.com>
2017-09-03 13:30:17 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,24 @@ setup (
 
        # Declare your packages' dependencies here, for eg:
        install_requires=[''],
+       
+       python_requires='>=3',
+       
+       classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Athletes',
+
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 3',  
+        ],
 
        # Fill in these to make your Egg ready for upload to
        # PyPI
